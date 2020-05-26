844e0dae3fbee2d3a00fdc8000110fb3bafed96c
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-05-26 19:23:42 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
-def parrot
-  puts "Squawk!"
-  "Squawk!"
+def parrot(phrase="Squawk")
+  puts "#{phrase}"
+  phrase
 end
 # returns the phrase
